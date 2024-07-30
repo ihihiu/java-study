d5be8ed744f8f17f6593abd84ebf45d5917b45a3
ihihiu <ihihiu98@gmail.com>
2024-07-30 15:11:44 +0900

    Sorting and Searching

---
+++ 06 Sorting and Searching/05. 중복 확인/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+        public Character solution(int n, int[] arr) {
+            Character answer = 'U';
+            Arrays.sort(arr);
+            for (int i = 0; i < n - 1; i++){
+                if (arr[i] == arr[i + 1]) return 'D';
+            }
+            return answer;
+       }
+
+
+        public static void main(String[] args) {
+            Main T = new Main();
+            Scanner kb = new Scanner(System.in);
+            int n = kb.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+            System.out.print(T.solution(n, arr));
+        }
+    }
