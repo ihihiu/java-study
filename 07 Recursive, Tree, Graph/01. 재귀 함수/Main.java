fb40718621ecca22e328d7b211d679ae910934dd
ihihiu <ihihiu98@gmail.com>
2024-07-31 17:54:42 +0900

    Recursive

---
+++ 07 Recursive, Tree, Graph/01. 재귀 함수/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public void DFS(int n){
+        if (n == 0) return;
+        else {
+            DFS(n - 1);
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        T.DFS(n);
+    }
+}
