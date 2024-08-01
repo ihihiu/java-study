a613b7049dd368853e563ca0ec9c95fe7c353cbe
ihihiu <ihihiu98@gmail.com>
2024-08-01 16:46:24 +0900

    Memoization

---
+++ 07 Recursive, Tree, Graph/04. 피보나치 수열/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static int[] fibo;
+    public int DFS(int n) {
+        if (fibo[n] != 0) return fibo[n];
+        if (n == 1 || n == 2) return fibo[n] = 1;
+        else return fibo[n] = DFS(n - 2) + DFS(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        fibo = new int[n + 1];
+        T.DFS(n);
+        for (int i = 1; i <= n; i++) System.out.print(fibo[i] + " ");
+
+    }
+}
